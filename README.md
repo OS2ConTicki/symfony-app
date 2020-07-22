5f88ff0d774c7002f2190e78cd46e0d1a837ad1f
Mikkel Ricky <rimi@aarhus.dk>
2020-07-22 11:20:46 +0200

    Added conference entity

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Uses https://github.com/OS2Conference/display-react for displaying data.
 
 ```sh
 docker-compose up -d
+symfony console doctrine:migrations:migrate --no-interaction
 symfony local:server:start
 ```
---
+++ src/Entity/Conference.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConferenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ConferenceRepository::class)
+ * @UniqueEntity(fields={"shortName"})
+ */
+class Conference
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="guid")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     */
+    private $shortName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Url()
+     */
+    private $dataUrl;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getShortName(): ?string
+    {
+        return $this->shortName;
+    }
+
+    public function setShortName(string $shortName): self
+    {
+        $this->shortName = $shortName;
+
+        return $this;
+    }
+
+    public function getDataUrl(): ?string
+    {
+        return $this->dataUrl;
+    }
+
+    public function setDataUrl(string $dataUrl): self
+    {
+        $this->dataUrl = $dataUrl;
+
+        return $this;
+    }
+}
